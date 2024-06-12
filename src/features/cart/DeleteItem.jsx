c5a0a2c5248c965888a0f9a8ac4c73a68f800f9c
CatalinVultur <catalin.vultur@amdaris.com>
2024-06-12 15:03:45 +0300

    - added deleting from cart functionality

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,15 @@
+import { useDispatch } from "react-redux";
+import { deleteItem } from "./cartSlice.js";
+import Button from "../../ui/Button.jsx";
+
+function DeleteItem({ pizzaId }) {
+    const dispatch = useDispatch();
+
+    return (
+        <Button type='small' onClick={() => dispatch(deleteItem(pizzaId))}>
+            Delete
+        </Button>
+    )
+}
+
+export default DeleteItem;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice.js";
 import { formatCurrency } from "../../utils/helpers.js";
 import Button from "../../ui/Button.jsx";
+import DeleteItem from "../cart/DeleteItem.jsx";
 
 function MenuItem({ pizza }) {
      const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
      const dispatch = useDispatch();
+     const currentQuantity = useSelector(getCurrentQuantityById(id));
+     const isInCart = currentQuantity > 0;
 
      function handleAddToCart() {
         const newItem = {
@@ -31,7 +34,11 @@ function MenuItem({ pizza }) {
                 <div className='mt-auto flex items-center justify-between'>
                     {!soldOut ? <p className='text-sm'>{formatCurrency(unitPrice)}</p> : <p className='text-sm font-medium uppercase text-stone-500'>Sold out</p>}
 
-                    {!soldOut &&
+                    {isInCart &&
+                        <DeleteItem pizzaId={id} />
+                    }
+
+                    {!soldOut && !isInCart &&
                         <Button type="small" onClick={handleAddToCart}>
                             Add to cart
                         </Button>
